fa6b1a23da1138cb289103bf5817e38e00169fc4
Ian Mckay <ian.mckay@kablamo.com.au>
2021-02-01 23:22:07 +1100

    Adjust the mapper

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const AWS = require('aws-sdk');
 const fs = require('fs'); 
 
-const iam_def = JSON.parse(fs.readFileSync('./iam_definition.json', {encoding:'utf8', flag:'r'})); 
+const iam_def = JSON.parse(fs.readFileSync('./iam_definition.json', {encoding:'utf8', flag:'r'}));
+const map = JSON.parse(fs.readFileSync('../map.json', {encoding:'utf8', flag:'r'}));
 
 sdk_services = []
 
@@ -11,14 +12,23 @@ for (let prop of Object.getOwnPropertyNames(AWS)) {
     }
 }
 
-console.log(sdk_services);
-
 iam_services = iam_def.map(x => x.prefix.toLowerCase());
 
 for (let sdk_service of sdk_services) {
-    if (!iam_services.includes(sdk_service.toLowerCase())) {
+    iam_service = null;
+    if (Object.keys(map['js_sdk_service_mappings']).includes(sdk_service)) {
+        iam_service = map['js_sdk_service_mappings'][sdk_service];
+    } else {
+        for (let service of iam_def) {
+            if (service.prefix.toLowerCase() == sdk_service.toLowerCase()) {
+                iam_service = service.prefix;
+            }
+        }
+    }
+    if (!iam_service) {
         console.log("Unmapped service: " + sdk_service);
     }
-}
 
+    console.dir(AWS);
+}
 
