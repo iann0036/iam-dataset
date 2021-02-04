1e215f07ec6e54ec16f9bb11be8f436b84df2ad3
Ian Mckay <ian.mckay@kablamo.com.au>
2021-02-04 21:07:25 +1100

    About half processed

--- js/index.js
+++ js/index.js
@@ -16,8 +16,8 @@ iam_services = iam_def.map(x => x.prefix.toLowerCase());
 
 for (let sdk_service of sdk_services) {
     iam_service = null;
-    if (Object.keys(map['js_sdk_service_mappings']).includes(sdk_service)) {
-        iam_service = map['js_sdk_service_mappings'][sdk_service];
+    if (Object.keys(map['sdk_service_mappings']).includes(sdk_service)) {
+        iam_service = map['sdk_service_mappings'][sdk_service];
     } else {
         for (let service of iam_def) {
             if (service.prefix.toLowerCase() == sdk_service.toLowerCase()) {
@@ -28,7 +28,46 @@ for (let sdk_service of sdk_services) {
     if (!iam_service) {
         console.log("Unmapped service: " + sdk_service);
     }
-
-    console.dir(AWS);
 }
 
+let methods = [];
+let unmapped_methods = [];
+
+for (let service of Object.keys(AWS.apiLoader.services)) {
+    for (let operation of Object.keys(AWS.apiLoader.services[service][Object.keys(AWS.apiLoader.services[service]).pop()]['operations'])) {
+        for (let sdkclass of Object.keys(AWS)) {
+            if (AWS[sdkclass]['serviceIdentifier'] == service) {
+                let fully_mapped = false;
+
+                methods.push(sdkclass + "." + operation);
+
+                if (map['sdk_method_iam_mappings'][sdkclass + "." + operation] && map['sdk_method_iam_mappings'][sdkclass + "." + operation][0]["action"] != "") {
+                    fully_mapped = true;
+                }
+
+                let mapped_iam_service = sdkclass;
+                if (map['sdk_service_mappings'][sdkclass]) {
+                    mapped_iam_service = map['sdk_service_mappings'][sdkclass];
+                }
+
+                for (let iam_service of iam_def) {
+                    if (iam_service['prefix'].toLowerCase() == mapped_iam_service.toLowerCase()) {
+                        for (let priv of iam_service["privileges"]) {
+                            if (priv.privilege == operation) {
+                                fully_mapped = true;
+                            }
+                        }
+                    }
+                }
+
+                if (!fully_mapped) {
+                    console.log(sdkclass + "." + operation);
+                    unmapped_methods.push(sdkclass + "." + operation);
+                }
+            }
+        }
+    }
+}
+
+console.log(methods.length);
+console.log(unmapped_methods.length);
