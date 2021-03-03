e13ad85b219c1235777241641f39497c094368b4
Ian Mckay <ian.mckay@kablamo.com.au>
2021-03-04 09:50:17 +1100

    Research

--- scratch.js
+++ scratch.js
@@ -28,3 +28,42 @@ for (var iamdefitem of iamdef) {
     }
 }
 
+// find dependency chains
+
+var mapdata = await fetch('https://raw.githubusercontent.com/iann0036/sdk-iam-map/main/map.json');
+map = await mapdata.json();
+
+var iamdefdata = await fetch('https://raw.githubusercontent.com/iann0036/sdk-iam-map/main/js/iam_definition.json');
+iamdef = await iamdefdata.json();
+
+var chains = [];
+
+for (var j=0; j<20; j++) {
+    for (var iamdefitem of iamdef) {
+        for (var priv of iamdefitem.privileges) {
+            for (var restype of priv.resource_types) {
+                if (restype['dependent_actions'].length > 0) {
+                    for (var depaction of restype['dependent_actions']) {
+                        var chainslength = chains.length;
+                        for (var i=0; i<chainslength; i++) {
+                            if (chains[i][0] == depaction) {
+                                chains[i].unshift(iamdefitem.prefix + ":" + priv.privilege);
+                            }
+                        }
+
+                        chains.push([iamdefitem.prefix + ":" + priv.privilege, depaction]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+chains = chains.filter(x => x.length > 2);
+
+let set  = new Set(chains.map(JSON.stringify));
+let arr2 = Array.from(set).map(JSON.parse);
+
+for (var el of arr2) {
+    console.log(el.join(","));
+}
