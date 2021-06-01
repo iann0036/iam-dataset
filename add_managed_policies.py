584b73c3c42fba8517bd030bab327392ff362739
Ian Mckay <contact@ianmckay.com.au>
2021-06-01 20:57:58 +1000

    Add extended policy logic

--- add_managed_policies.py
+++ add_managed_policies.py
@@ -21,6 +21,8 @@ policies = []
 
 for policyname in os.listdir("MAMIP/policies/"):
     policy = {}
+    detailed_policy = {}
+
     with open("MAMIP/policies/{}".format(policyname), "r") as f:
         policy = json.loads(f.read())
 
@@ -38,7 +40,9 @@ for policyname in os.listdir("MAMIP/policies/"):
         policy['PolicyVersion']['Document']['Statement'] = [policy['PolicyVersion']['Document']['Statement']]
 
     access_levels = []
-    unknown_actions = False
+    effective_actions = []
+
+    unknown_actions = []
     for statement in policy['PolicyVersion']['Document']['Statement']:
         if 'Action' in statement:
             if not isinstance(statement['Action'], list):
@@ -51,8 +55,20 @@ for policyname in os.listdir("MAMIP/policies/"):
                     if re.search(matchexpression.lower(), potentialaction.lower()):
                         access_levels.append(allactions[potentialaction])
                         foundmatch = True
+
+                        condition = None
+                        if 'Condition' in statement:
+                            condition = statement['Condition']
+
+                        effective_actions.append({
+                            'action': action,
+                            'effective_action': potentialaction,
+                            'access_levels': allactions[potentialaction],
+                            'condition': condition
+                        })
+
                 if not foundmatch:
-                    unknown_actions = True
+                    unknown_actions.append(action)
 
         elif 'NotAction' in statement:
             print("Missing Action in " + policyname)
@@ -68,9 +84,26 @@ for policyname in os.listdir("MAMIP/policies/"):
         'updatedate': updatedate,
         'version': policy['PolicyVersion']['VersionId'],
         'malformed': malformed,
-        'unknown_actions': unknown_actions,
+        'unknown_actions': (len(unknown_actions) > 0),
         'access_levels': access_levels
     })
+
+    detailed_policy = {
+        'name': policyname,
+        'deprecated': (policyname in deprecated_policies),
+        'createdate': policy['PolicyVersion']['CreateDate'],
+        'updatedate': updatedate,
+        'version': policy['PolicyVersion']['VersionId'],
+        'malformed': malformed,
+        'unknown_actions': unknown_actions,
+        'access_levels': access_levels,
+        'document': policy['PolicyVersion']['Document'],
+        'effective_actions': effective_actions,
+        'unknown_actions': unknown_actions
+    }
+
+    with open("managedpolicies/{}.json".format(policyname), "w") as f:
+        f.write(json.dumps(detailed_policy))
 
 with open("managed_policies.json", "w") as f:
     f.write(json.dumps({
