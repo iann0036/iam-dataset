8ba2760c0b08864a139a1b46f134fbb794be61d7
Ian Mckay <contact@ianmckay.com.au>
2021-09-05 14:08:48 +1000

    Better merge

--- patch_iam_definition.py
+++ patch_iam_definition.py
@@ -32,10 +32,59 @@ for root in undocumented_roots:
                 break
 
 # APIGW Merge
-for i in range(len(iam_def)):
-    if iam_def[i]['service_name'] == 'Amazon API Gateway Management V2':
-        iam_def.pop(i)
-        break
+i = 0
+while i < len(iam_def):
+    j = i + 1
+    while j < len(iam_def):
+        if iam_def[i]['prefix'] == iam_def[j]['prefix']:
+            # merge conditions
+            for condition in iam_def[i]['conditions']:
+                found = False
+                for merge_condition in iam_def[j]['conditions']:
+                    if condition['condition'] == merge_condition['condition']:
+                        found = True
+                        break
+                if not found:
+                    iam_def[i]['conditions'].append(merge_condition)
+
+            # merge privileges
+            k = 0
+            while k < len(iam_def[i]['privileges']):
+                found = False
+                for merge_privilege in iam_def[j]['privileges']:
+                    if iam_def[i]['privileges'][k]['privilege'] == merge_privilege['privilege']:
+                        for merge_resource_type in merge_privilege['resource_types']:
+                            found2 = False
+                            for resource_type in iam_def[i]['privileges'][k]['resource_types']:
+                                if merge_resource_type['resource_type'].replace("*", "") == resource_type['resource_type'].replace("*", ""):
+                                    found2 = True # TODO: better merge for same resource type
+                                    break
+                            if not found2:
+                                iam_def[i]['privileges'][k]['resource_types'].append(merge_resource_type)
+                        found = True
+                        break
+                if not found:
+                    iam_def[i]['privileges'].append(merge_privilege)
+                k += 1
+
+            # merge resources
+            k = 0
+            while k < len(iam_def[i]['resources']):
+                found = False
+                for merge_resource in iam_def[j]['resources']:
+                    if iam_def[i]['resources'][k]['arn'] == merge_resource['arn']:
+                        iam_def[i]['resources'][k]['condition_keys'] = list(set(iam_def[i]['resources'][k]['condition_keys'] + merge_resource['condition_keys']))
+                        found = True
+                        break
+                if not found:
+                    iam_def[i]['resources'].append(merge_resource)
+                k += 1
+
+            print("Merged " + iam_def[j]['service_name'] + " (" + iam_def[j]['prefix'] + ")")
+            iam_def.pop(j)
+            j -= 1
+        j += 1
+    i += 1
 
 # Renames
 for i in range(len(iam_def)):
@@ -43,6 +92,18 @@ for i in range(len(iam_def)):
         iam_def[i]['service_name'] = 'Amazon RDS, Neptune & DocumentDB'
     if iam_def[i]['prefix'] == 'apigateway':
         iam_def[i]['service_name'] = 'Amazon API Gateway Management'
+    if iam_def[i]['prefix'] == 'aws-marketplace':
+        iam_def[i]['service_name'] = 'AWS Marketplace'
+    if iam_def[i]['prefix'] == 'elasticloadbalancing':
+        iam_def[i]['service_name'] = 'Elastic Load Balancing'
+    if iam_def[i]['prefix'] == 'kinesisanalytics':
+        iam_def[i]['service_name'] = 'Amazon Kinesis Analytics'
+    if iam_def[i]['prefix'] == 'lex':
+        iam_def[i]['service_name'] = 'Amazon Lex'
+    if iam_def[i]['prefix'] == 'ses':
+        iam_def[i]['service_name'] = 'Amazon SES & Pinpoint'
+    if iam_def[i]['prefix'] == 'greengrass':
+        iam_def[i]['service_name'] = 'AWS IoT Greengrass'
 
 # Undocumented method tagging
 for k, v in mapdata['sdk_method_iam_mappings'].items():
