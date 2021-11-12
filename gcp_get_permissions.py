83848ce9285ddbf17c4b47077d964c842357be31
Ian Mckay <contact@ianmckay.com.au>
2021-11-12 23:15:27 +1100

    Add GCP permissions get

---
+++ gcp_get_permissions.py
@@ -0,0 +1,28 @@
+import os
+import json
+import time
+import requests
+import re
+from bs4 import BeautifulSoup
+
+base_ref_page = requests.get("https://cloud.google.com/iam/docs/permissions-reference").text
+frame_page_url = re.search('<iframe src="([^"]+)"', base_ref_page).group(1)
+if frame_page_url[0] == "/":
+    frame_page_url = "https://cloud.google.com" + frame_page_url
+frame_page = requests.get(frame_page_url).text
+parsed_frame_page = BeautifulSoup(frame_page)
+
+result = {}
+
+for row in parsed_frame_page.find('tbody').find_all('tr'):
+    permission = row.find_all('td')[0].get('id')
+    result[permission] = []
+    for role in row.find_all('td')[1].find_all('li'):
+        result[permission].append({
+            'id': role.find('code').decode_contents(),
+            'name': role.decode_contents().split(" (")[0]
+        })
+
+with open("gcp/permissions.json", "w") as f:
+    f.write(json.dumps(result, indent=2, sort_keys=True))
+    
