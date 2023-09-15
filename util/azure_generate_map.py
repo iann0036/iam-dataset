c374d64f8d2ff060dd026d5c162ad69029f5e1e1
Ian Mckay <contact@ianmckay.com.au>
2023-09-16 01:41:00 +1000

    Update Azure map

--- util/azure_generate_map.py
+++ util/azure_generate_map.py
@@ -79,51 +79,51 @@ for opservice in ops:
 
                         # 2:2
                         if len(method_parts) == 2 and len(opname_parts) == 2 and method_parts[0].lower() == opname_parts[0].lower() and opname_parts[1].lower() == "read" and method_parts[1].lower() in ["list", "get"]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 2 and method_parts[0].lower() == opname_parts[0].lower() and opname_parts[1].lower() == "delete" and method_parts[1].lower() in ["delete"]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 2 and method_parts[0].lower() == opname_parts[0].lower() and opname_parts[1].lower() == "write" and method_parts[1].lower() in ["create", "update", "createorupdate"]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
 
                         # 3:2
                         if len(method_parts) == 2 and len(opname_parts) == 3 and method_parts[0].lower() == opname_parts[0].lower() and opname_parts[2].lower() == "read" and method_parts[1].lower() in ["list"+opname_parts[1].lower(), "get"+opname_parts[1].lower()]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 3 and method_parts[0].lower() == opname_parts[0].lower() and opname_parts[2].lower() == "delete" and method_parts[1].lower() in ["delete"+opname_parts[1].lower()]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 3 and method_parts[0].lower() == opname_parts[0].lower() and opname_parts[2].lower() == "write" and method_parts[1].lower() in ["create"+opname_parts[1].lower(), "update"+opname_parts[1].lower(), "createorupdate"+opname_parts[1].lower()]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 3 and method_parts[0].lower() == opname_parts[0].lower() and method_parts[1].lower() == opname_parts[1].lower() and opname_parts[2].lower() == "action":
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
 
                         method_parts0plural = p.plural_noun(method_parts[0].lower())
 
                         # 2:2 plural
                         if len(method_parts) == 2 and len(opname_parts) == 2 and method_parts0plural == opname_parts[0].lower() and opname_parts[1].lower() == "read" and method_parts[1].lower() in ["list", "get"]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 2 and method_parts0plural == opname_parts[0].lower() and opname_parts[1].lower() == "delete" and method_parts[1].lower() in ["delete"]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 2 and method_parts0plural == opname_parts[0].lower() and opname_parts[1].lower() == "write" and method_parts[1].lower() in ["create", "update", "createorupdate"]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
 
                         # 3:2 plural [Location_ListCapabilities == locations/capabilities/read]
                         if len(method_parts) == 2 and len(opname_parts) == 3 and method_parts0plural == opname_parts[0].lower() and opname_parts[2].lower() == "read" and method_parts[1].lower() in ["list"+opname_parts[1].lower(), "get"+opname_parts[1].lower()]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 3 and method_parts0plural == opname_parts[0].lower() and opname_parts[2].lower() == "delete" and method_parts[1].lower() in ["delete"+opname_parts[1].lower()]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
                         elif len(method_parts) == 2 and len(opname_parts) == 3 and method_parts0plural == opname_parts[0].lower() and opname_parts[2].lower() == "write" and method_parts[1].lower() in ["create"+opname_parts[1].lower(), "update"+opname_parts[1].lower(), "createorupdate"+opname_parts[1].lower()]:
-                            candidates.append(op['name'])
+                            candidates.append(op)
                             continue
 
                     # Entity array matching
@@ -140,6 +140,9 @@ for opservice in ops:
                         if trimmed_pathname.startswith("/subscriptions/{subscriptionId}"):
                             scope_subscription = True
                             trimmed_pathname = trimmed_pathname[len("/subscriptions/{subscriptionId}"):]
+                        if trimmed_pathname.startswith("/Subscriptions/{subscriptionId}"):
+                            scope_subscription = True
+                            trimmed_pathname = trimmed_pathname[len("/Subscriptions/{subscriptionId}"):]
                         scope_resourcegroup = False
                         if trimmed_pathname.startswith("/resourceGroups/{resourceGroupName}"):
                             scope_resourcegroup = True
@@ -225,7 +228,7 @@ for opservice in ops:
                                     path_entries_singular_join.lower(),
                                     path_entries_singular_join_noslashes.lower()
                                 ]:
-                                    candidates.append(op['name'])
+                                    candidates.append(op)
                                     continue
                     
                     ## End
@@ -234,9 +237,12 @@ for opservice in ops:
                             result[httpmethodname.upper()] = {}
                         if pathname not in result[httpmethodname.upper()]:
                             result[httpmethodname.upper()][pathname] = {}
-                        if candidates[0] not in result[httpmethodname.upper()][pathname]:
-                            result[httpmethodname.upper()][pathname][candidates[0]] = {}
-                        result[httpmethodname.upper()][pathname][candidates[0]]['automated'] = True
+                        if candidates[0]['name'] not in result[httpmethodname.upper()][pathname]:
+                            result[httpmethodname.upper()][pathname][candidates[0]['name']] = {}
+                        result[httpmethodname.upper()][pathname][candidates[0]['name']]['automated'] = True
+                        if candidates[0]['isDataAction']:
+                            result[httpmethodname.upper()][pathname][candidates[0]['name']]['isDataAction'] = True
+
 
 with open("azure/map.json", "w") as f:
     f.write(json.dumps(result, indent=2, sort_keys=True))
