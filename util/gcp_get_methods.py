e357b4e076c7d7feaec79e49e582f9e5a5fc6c42
Ian Mckay <contact@ianmckay.com.au>
2025-03-19 21:43:28 +1100

    Adjust flatPath for GCP

--- util/gcp_get_methods.py
+++ util/gcp_get_methods.py
@@ -14,13 +14,17 @@ def resources_recurse(resources, resource_type_path):
             for method_name in resources[resource_type]['methods'].keys():
                 method_id = resources[resource_type]['methods'][method_name]['id']
                 if method_id not in result[api['name']]['methods']:
+                    flatpath = resources[resource_type]['methods'][method_name]['flatPath'] if 'flatPath' in resources[resource_type]['methods'][method_name] else resources[resource_type]['methods'][method_name]['path']
+                    if flatpath.startswith(apidetail['version'] + "/"):
+                        flatpath = "\{_version\}" + flatpath[len(apidetail['version']):]
+
                     result[api['name']]['methods'][method_id] = {
                         'description': resources[resource_type]['methods'][method_name]['description'] if 'description' in resources[resource_type]['methods'][method_name] else '',
                         'httpMethod': resources[resource_type]['methods'][method_name]['httpMethod'],
                         'method': method_name,
                         'resourceType': resource_type,
                         'resourceTypePath': resource_type_path,
-                        'flatPath': resources[resource_type]['methods'][method_name]['flatPath'] if 'flatPath' in resources[resource_type]['methods'][method_name] else resources[resource_type]['methods'][method_name]['path'],
+                        'flatPath': flatpath,
                         'versions': []
                     }
                     result_ext[api['name']]['methods'][method_id] = {
@@ -29,7 +33,7 @@ def resources_recurse(resources, resource_type_path):
                         'method': method_name,
                         'resourceType': resource_type,
                         'resourceTypePath': resource_type_path,
-                        'flatPath': resources[resource_type]['methods'][method_name]['flatPath'] if 'flatPath' in resources[resource_type]['methods'][method_name] else resources[resource_type]['methods'][method_name]['path'],
+                        'flatPath': flatpath,
                         'versions': [],
                         'parameters': resources[resource_type]['methods'][method_name]['parameters'] if 'parameters' in resources[resource_type]['methods'][method_name] else {}
                     }
